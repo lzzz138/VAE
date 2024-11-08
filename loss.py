6cc3c89ca07ff4a4b585c636fdb26d9c23e78e4e
Reshape <1742809983@qq.com>
2024-11-08 19:45:58 +0800

    11.08 提交VAE代码

--- loss.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn.functional as F
-
-def loss_function(x_hat, x, u, log_var):
-    BCE = F.binary_cross_entropy(x_hat, x, reduction='sum')
-    KLD = 0.5 * torch.sum(torch.exp(log_var) + torch.pow(u, 2) - 1. - log_var)
-    loss = BCE + KLD
-    return loss, BCE, KLD
